f709e6276938ae30b06515746a8d29e137d014d1
sampathKumaryemjala <sampathyemjala@gmail.com>
2025-11-07 18:53:03 +0530

    user model

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,14 +1,23 @@
 import {ApiResponse} from '../utils/api-response.js'
-
-const healthCheck =(req,res)=>{
+import { asyncHandler } from '../utils/async-handler.js'
+/*
+const healthCheck =async (req,res,next)=>{
     try {
+        const user = await getUserFromDB()
         res.status(200).json(
             new ApiResponse(200,{message:"server is running "})
         )
     } catch (error) {
-        
+        next(error)
     }
-
 }
+
+*/
+
+const healthCheck =asyncHandler(async (req,res)=>{
+    res.status(200)
+    .json(new ApiResponse(200,{message:"server is still running"}));
+})
+
 
 export {healthCheck}
